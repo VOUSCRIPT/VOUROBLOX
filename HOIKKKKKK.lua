41842f32f6baf114042121dc8dcce1487986d6ea
VOUSCRIPT <kinglnnn886@gmail.com>
2025-02-13 14:04:49 +0800

    Create HOIKKKKKK.lua

---
+++ HOIKKKKKK.lua
@@ -0,0 +1,4 @@
+   local v1 = require(game.ReplicatedStorage.Modules.Common)
+    local l_UIHandle_0 = require(game:GetService("ReplicatedStorage").Modules.Client.Hatch.UIHandle)
+    local path = game:GetService("Players").LocalPlayer.PlayerGui.HatchGuis
+    local Hatch = require(game:GetService("ReplicatedStorage").Modules.Client.Hatch)
