7e1a96995c39639e54b51d7c3f40def7b44e9db1
VOUSCRIPT <kinglnnn886@gmail.com>
2025-07-01 14:34:18 +0800

    Update 3008 [2,73].lua

--- 3008 [2,73].lua
+++ 3008 [2,73].lua
@@ -1,4 +1,4 @@
-
+if "霖溺-3008" == LinniHubScript then
 local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
 local LocalPlayer = Players.LocalPlayer
@@ -127,4 +127,10 @@ if LocalPlayer.Character then
     initESP()
 else
     LocalPlayer.CharacterAdded:Connect(initESP)
+end
+
+wait(0.1)
+else
+setclipboard("霖溺QQ新主群：http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=viOjjgj19-TUiZlamhpxb6uvWwVNGoB7&authKey=ACDi9sCtIis%2F4P%2BtirP046uWaJ54%2F149eBnUvaAsMu%2BWZwSFoEQrzZC9UDGFwmp%2F&noverify=0&group_code=744830231")
+game.Players.LocalPlayer:Kick("你没复制全，中文也是的❌")
 end
