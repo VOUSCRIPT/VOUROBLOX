48073e1858939a4400a4532143144b4f73555eb2
VOUSCRIPT <kinglnnn886@gmail.com>
2025-07-04 10:21:18 +0800

    Add files via upload

---
+++ 2-Player-Brainrot-Tycoon.lua
@@ -0,0 +1,9 @@
+if "霖溺-2-Player-Brainrot-Tycoon" == LinniHubScript then
+
+local args = { [1] = 99999999999999 } game:GetService("ReplicatedStorage"):WaitForChild("GetFriendCount"):FireServer(unpack(args))
+
+wait(0.1)
+else
+setclipboard("霖溺QQ新主群：http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=viOjjgj19-TUiZlamhpxb6uvWwVNGoB7&authKey=ACDi9sCtIis%2F4P%2BtirP046uWaJ54%2F149eBnUvaAsMu%2BWZwSFoEQrzZC9UDGFwmp%2F&noverify=0&group_code=744830231")
+game.Players.LocalPlayer:Kick("你没复制全，中文也是的❌")
+end
